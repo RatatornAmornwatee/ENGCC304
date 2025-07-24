636dd6f079822b089c43d929493a39fc9a82c4c2
Ratatorn Amornwatee <ratatorn.am@gmail.com>
2025-07-24 13:34:39 +0700

    Update main.c

--- Lab/Lab4/main.c
+++ Lab/Lab4/main.c
@@ -2,20 +2,20 @@
 
 int main() {
 char emplyeeid[10] ;
-	int workinghrs ;
-	float salary ;
+    int workinghrs ;
+    float salary ;
 
-	printf( "Input the Employees ID(Max. 10 chars): \n" ) ;
-	scanf( "%s" , &emplyeeid ) ;
+    printf( "Input the Employees ID(Max. 10 chars): \n" ) ;
+    scanf( "%s" , &emplyeeid ) ;
 
-	printf( "Input the working hrs: \n" ) ;
-	scanf( "%d" , &workinghrs ) ;
+    printf( "Input the working hrs: \n" ) ;
+    scanf( "%d" , &workinghrs ) ;
 
-	printf( "Salary amount/hr: \n" ) ;
-	scanf( "%f" , &salary ) ;
+    printf( "Salary amount/hr: \n" ) ;
+    scanf( "%f" , &salary ) ;
 
-	printf( "Employees ID = %s \n" , emplyeeid ) ;
-	printf( "Salary = U$ %.2f" , workinghrs * salary ) ;
+    printf( "Employees ID = %s \n" , emplyeeid ) ;
+    printf( "Salary = U$ %.2f" , workinghrs * salary ) ;
   
-	return 0 ;
+    return 0 ;
 }//end function
