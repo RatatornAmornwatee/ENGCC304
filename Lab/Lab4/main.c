12357eb444fe9cb6d12cbd3f5d8f2497e676be71
Ratatorn Amornwatee <ratatorn.am@gmail.com>
2025-07-24 13:26:34 +0700

    Add files via upload

--- Lab/Lab4/main.c
+++ Lab/Lab4/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
+  char emplyeeid[10] ;
+  int workinghrs ;
+  float salary ;
+
+  printf( "Input the Employees ID(Max. 10 chars): \n" ) ;
+  scanf( "%s" , &emplyeeid ) ;
+
+  printf( "Input the working hrs: \n" ) ;
+  scanf( "%d" , &workinghrs ) ;
+
+  printf( "Salary amount/hr: \n" ) ;
+  scanf( "%f" , &salary ) ;
+
+  printf( "Employees ID = %s \n" , emplyeeid ) ;
+  printf( "Salary = U$ %.2f" , workinghrs * salary ) ;
   
-  return 0;
-}
+  return 0 ;
+}//end function
