4e70a9eef65669ddc9879c77df25ec03a4c55650
Ratatorn Amornwatee <ratatorn.am@gmail.com>
2025-07-31 12:21:47 +0700

    Add files via upload

--- Lab/Lab6/main.c
+++ Lab/Lab6/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
-}
+  int N = -1 ;
+  printf( "Enter value: \n" ) ;
+  scanf( "%d" , &N ) ;
+
+  printf( "Series: " ) ;
+  if( N % 2 == 0 ) {
+    for( int i = N ; i >= 0 ; i-=2 ) {
+      printf( "%d " , i ) ;
+    }//end for
+  }
+  else if ( N % 2 > 0) {
+    for( int i = 1 ; i <= N ; i+=2 ) {
+      printf( "%d " , i ) ;
+    }// end for
+  }else{
+    printf( "please enter number only." );
+  }//end if
+
+  return 0 ;
+}//end function
