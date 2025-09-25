7437aa49122b3b03bfa6a619852c00f17ccd3538
Ratatorn Amornwatee <ratatorn.am@gmail.com>
2025-09-25 11:36:47 +0700

    Update main.c

--- Lab/Lab10/main.c
+++ Lab/Lab10/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+
+void Ispalindrome( char text[] );
 
 int main() {
+  char text[] = {} ;
+
+  printf( "Enter word:\n" );
+  scanf( "%s" , text ) ;
+
+  Ispalindrome( text ) ;
+}//end function
+
+void Ispalindrome( char text[] ) {
+  int i = 0 ;
+  int j = strlen( text ) - 1 ;
+
+  if( strlen(text ) % 2 == 0 ){
+    printf( "Not Pass." ) ;
+    return ;
+  }//end if
   
-  return 0;
-}
+  while( i == j ) {
+    if ( text[i] != text[j] ) {
+      printf( "Not Pass." ) ;
+      return ;
+    }//end function
+    i++ ;
+    j-- ;
+  }//end while
+  printf( "Pass." ) ; 
+}//end function
