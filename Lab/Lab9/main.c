f4feac7ed2e1cba17d264a65ff77a6c2b87031de
Ratatorn Amornwatee <ratatorn.am@gmail.com>
2025-09-25 11:37:18 +0700

    Update main.c

--- Lab/Lab9/main.c
+++ Lab/Lab9/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
-}
+  int n ;
+  int i = 0 ;
+  int j = 0 ;
+
+  printf( "Enter N : \n" ) ;
+  scanf( "%d" , &n ) ;
+
+  int arr[n];
+
+  for( i = 0 ; i < n ; i++ ) {
+    printf( "Enter value[%d] :\n" ,i ) ;
+    scanf( "%d" , &arr[i] );
+  }//end for
+
+  printf( "Index: " ) ;
+  for( i = 0 ; i < n ; i++ ) {
+    printf( "%d " ,i ) ;
+  }//end for
+
+  printf( "\nArray: " );
+  for( i = 0 ; i < n ; i++ ) {
+    for( j = 2 ; j * j <= arr[i] ; j++ ) {
+      if( arr[i] % j == 0) break ;
+    }//end for
+
+    if( j * j <= arr[i] ){
+      printf( "# " ) ;
+    }
+    else{
+      printf( "%d " , arr[i] ) ;
+    }//end if
+  }//end for
+}//end function
